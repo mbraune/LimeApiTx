2ae18b1abd08912c17e2748616354165b844bfea
Martin Braune <martin@int64.de>
2020-03-05 00:15:31 +0100

    testcase defines in header file

--- LimeTxCw/LimeTxCw.cpp
+++ LimeTxCw/LimeTxCw.cpp
@@ -7,12 +7,26 @@
 #include <algorithm>
 #include "lime/LimeSuite.h"
 
+enum Testcases {
+#   define TC(a) a,
+#   include "testcases.h"
+#   undef TC
+    TcCount
+};
+
+char const* const testcases_str[] = {
+#   define TC(a) #a,
+#   include "testcases.h"
+#   undef TC
+    0
+};
+
 
 using namespace std;
 
 static lms_device_t* device = NULL;
 static unsigned int gaindB = 0;
-static float_type  freq = 1.1;
+static float_type  dFreq = 1.1;
 
 int device_error()
 {
@@ -26,13 +40,13 @@ int device_error()
 
 void show_usage()
 {
-    cout << "LimeTxCw  v0.201" << endl;
+    cout << "LimeTxCw  v0.205" << endl;
     cout << "\tread cmds     :  help , info , stat , temp=?, " << endl;
     cout << "\tctrl cmds     :  txon , txoff , gain=[0..70], freq=[MHz] " << endl;
     cout << "\tmisc cmd      :  clr " << endl;
     cout << "\texit          :  stop " << endl;
     cout << "------------------------------------------------------------ " << endl;
-    cout << "cmd_ok\n";
+    //cout << "cmd_ok\n";
 }
 
 void show_info()
@@ -45,7 +59,7 @@ void show_info()
     ss << "\t" << devInfo->deviceName << "   serial " << hex << uppercase << devInfo->boardSerialNumber << endl;
     cout << ss.str();
     cout << "\thw version " << devInfo->hardwareVersion << "   fw version " << devInfo->firmwareVersion << endl;
-    cout << "cmd_ok\n";
+    //cout << "cmd_ok\n";
 }
 
 void show_status()
@@ -69,21 +83,96 @@ void show_status()
     ss << "   gain = " << mygain << " dB" << endl;
 
     cout << ss.str();
+}
+
+//
+// convert string to Testcase enum
+// if not found return none
+Testcases getTestcase(string s) {
+    int i; 
+    for (i = 0; i < TcCount; i++) {
+        if (s.find(testcases_str[Testcases(i)]) != string::npos)
+            return static_cast<Testcases>(i);
+    }
+    return none;
+}
+
+// helper
+// cmd including "=?" is query
+bool is_query(const string cmd) {
+    return (cmd.find("=?") != string::npos);
+}
+
+//############################################
+// handle_cmd  functions
+//  --first check if only query is allowed, 
+//  --todo parse parameter
+
+// execute system command 
+//e.g. syscmd=cls
+// syscmd=ls calls system("ls")
+void handle_syscmd(string s) {
+    string ss;
+    s.erase(0, 7);
+    ss = "\"" + s + "\"";
+    std::system(ss.c_str());
     cout << "cmd_ok\n";
 }
 
+void handle_temp(const string s){
+    // only query valid
+    float_type temp;
+    if (LMS_GetChipTemperature(device, 0, &temp) < 0)
+        device_error();
+     cout << "\t" << temp << endl << "cmd_ok" << endl;
+}
+
+void handle_devid(const string s) {
+    // only query valid
+    stringstream ss;
+    const lms_dev_info_t* devInfo;
+    devInfo = LMS_GetDeviceInfo(device);
+    ss << "\t" << hex << uppercase << devInfo->boardSerialNumber << endl  << "cmd_ok" << endl;
+    cout << ss.str();
+}
+
+void handle_enab_chan(const string s) {
+    // only write valid, 2 parameter + on/off required
+    // e.g. enab_chan,1,0=1
+    //      enab_chan,1,0=0
+    std::size_t pos = s.find("=");
+    std::string str3 = s.substr(pos+1);
+    bool bCmd = stoi(str3);
+    //std::string delimiter = ",";
+    LMS_EnableChannel(device, LMS_CH_TX, 0, bCmd);
+    cout << "cmd_ok" << endl;
+}
+
+void handle_reset() {
+    LMS_Reset(device);
+    cout << "cmd_ok" << endl;
+}
+
+void handle_close() {
+    LMS_Close(device);
+    cout << "cmd_ok" << endl;
+}
+
+// ##########################################################
 
 int main(int argc, char** argv)
 {
     show_usage();
 
     // initial values
-    freq = 1575.42e6;
+    dFreq = 1575.42e6;
     gaindB = 20;
 
     int n = LMS_GetDeviceList(nullptr);
     if (n > 0)
     {
+        // initialization, 
+        // print some info
         if (LMS_Open(&device, NULL, NULL) != 0) //open first device
             device_error();
 
@@ -95,7 +184,7 @@ int main(int argc, char** argv)
         if (LMS_EnableChannel(device, LMS_CH_TX, 0, true) != 0)
             device_error();
 
-        if (LMS_SetLOFrequency(device, LMS_CH_TX, 0, freq) != 0)
+        if (LMS_SetLOFrequency(device, LMS_CH_TX, 0, dFreq) != 0)
             device_error();
 
         if (LMS_SetGaindB(device, LMS_CH_TX, 0, gaindB) != 0)
@@ -106,22 +195,36 @@ int main(int argc, char** argv)
 
         show_status();
 
+        cout << "cmd_ok\n";
+
         string cmd;
         while (getline(cin, cmd))
         {
             cmd.erase(std::remove(cmd.begin(), cmd.end(), ' '), cmd.end());
 
-            if      (cmd == "help") show_usage();
-            else if (cmd == "info") show_info();
-            else if (cmd == "stat") show_status();
-            else if (cmd == "temp=?") {
-                float_type temp;
-                if (LMS_GetChipTemperature(device, 0, &temp) < 0)
-                    device_error();
-                cout << "\ttemperature = " << temp << endl;
-                cout << "cmd_ok\n";
+            // valid cmd ?
+            Testcases eTC = getTestcase(cmd);
+            if (eTC == none) {
+                cout << "err_1000\n";
+                break;
             }
-            else if (cmd == "txon") {
+
+            // else parse parameter
+            switch (eTC) {
+                case syscmd:    handle_syscmd(cmd);     break;
+                case help:      show_usage();           break;
+                case antenna:   show_status();          break;
+                case temp:      handle_temp(cmd);       break;
+                case devid:     handle_devid(cmd);      break;
+
+                case enab_chan: handle_enab_chan(cmd);  break;
+                case close:     handle_close();         break;
+                case reset:     handle_reset();         break;
+
+                default: break;
+            }
+
+            if (cmd == "txon") {
                 LMS_EnableChannel(device, LMS_CH_TX, 0, true);
                 show_status();
             }
@@ -148,21 +251,7 @@ int main(int argc, char** argv)
                     else cout << "cmd_ok\n";
                 }
             }
-            else if (cmd == "clr") {
-                system("CLS");
-                show_usage();
-            }
-            else if (cmd == "stop") {
-                LMS_Reset(device);
-                if (LMS_Close(device) == 0) {
-                    cout << "Closed" << endl;
-                    cout << "cmd_ok\n";
-                }
-                break;
-            }
-            else {
-                cout << "err_1000\n";   // unknown cmd
-            }
+
         }
     }
     else {
---
+++ LimeTxCw/testcases.h
@@ -0,0 +1,21 @@
+// testcases.h
+// define macros 
+//
+
+//-------------
+TC(none)
+TC(syscmd)
+TC(help)
+TC(antenna)
+TC(ant_bw)
+TC(temp)
+TC(devid)
+TC(freq)
+TC(gain_db)
+TC(enab_chan)
+TC(samp_rate)
+TC(loadcnf)
+TC(savecnf)
+TC(clock_frq)
+TC(reset)
+TC(close)
